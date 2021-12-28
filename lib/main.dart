7934e383e3600eed8090eb4d36a27d58839c619b
kard <kard.sahaphong@refinitiv.com>
2021-12-28 12:44:04 +0700

    ep17_list_of_data

--- lib/main.dart
+++ lib/main.dart
@@ -1,105 +1,35 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MaterialApp(
-      home: NinjaCard(),
+void main() => runApp(MaterialApp(
+      home: QuoteList(),
     ));
 
-class NinjaCard extends StatefulWidget {
-  const NinjaCard({Key? key}) : super(key: key);
+class QuoteList extends StatefulWidget {
+  QuoteList({Key? key}) : super(key: key);
 
   @override
-  State<NinjaCard> createState() => _NinjaCardState();
+  _QuoteListState createState() => _QuoteListState();
 }
 
-class _NinjaCardState extends State<NinjaCard> {
-  int ninjaLevel = 0;
+class _QuoteListState extends State<QuoteList> {
+  List<String> quotes = [
+    'Be yourself; everyone else is already taken',
+    'I have nothing yo declare except my genius',
+    'The truth is rarely pure and never simple'
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.grey[900],
-        appBar: AppBar(
-          title: const Text('Ninja ID Card'),
-          centerTitle: true,
-          backgroundColor: Colors.grey[850],
-          elevation: 0.0,
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () => {
-            setState(() {
-              ninjaLevel++;
-            })
-          },
-          child: const Icon(Icons.add),
-          backgroundColor: Colors.grey[800],
-        ),
-        body: Padding(
-          padding: const EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              const Center(
-                child: CircleAvatar(
-                  backgroundImage: AssetImage('assets/chunLi.png'),
-                  radius: 40.0,
-                ),
-              ),
-              Divider(
-                height: 60.0,
-                color: Colors.grey[800],
-              ),
-              const Text(
-                'Name',
-                style: TextStyle(
-                  color: Colors.grey,
-                  letterSpacing: 2.0,
-                ),
-              ),
-              const SizedBox(height: 10.0),
-              const Text(
-                'Chun-Li',
-                style: TextStyle(
-                  color: Colors.amberAccent,
-                  letterSpacing: 2.0,
-                  fontSize: 28.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const SizedBox(height: 10.0),
-              const Text(
-                'CURRENT NINJA LEVEL',
-                style: TextStyle(
-                  color: Colors.grey,
-                  letterSpacing: 2.0,
-                ),
-              ),
-              const SizedBox(height: 10.0),
-              Text(
-                '$ninjaLevel',
-                style: const TextStyle(
-                  color: Colors.amberAccent,
-                  letterSpacing: 2.0,
-                  fontSize: 28.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const SizedBox(height: 20.0),
-              Row(
-                children: <Widget>[
-                  Icon(Icons.email, color: Colors.grey[400]),
-                  const SizedBox(height: 10.0),
-                  Text(
-                    'chun.li@thenetninja.co.uk',
-                    style: TextStyle(
-                      color: Colors.grey[400],
-                      fontSize: 18.0,
-                      letterSpacing: 1.0,
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ));
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        title: const Text('Awesome Quotes'),
+        centerTitle: true,
+        backgroundColor: Colors.redAccent,
+      ),
+      body: Column(
+        children: quotes.map((quote) => Text(quote)).toList(),
+      ),
+    );
   }
 }
