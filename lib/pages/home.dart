f7bdced546e014966330d9217a7d9cae5a257730
kard <kard.sahaphong@refinitiv.com>
2021-12-29 17:50:52 +0700

    ep31_formatting_and_showing_dates

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class Home extends StatefulWidget {
   Home({Key? key}) : super(key: key);
@@ -12,18 +13,41 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-    Object? data = ModalRoute.of(context)?.settings.arguments;
+    Map? data = ModalRoute.of(context)?.settings.arguments as Map;
 
     return Scaffold(
-      body: SafeArea(
-        child: Column(children: <Widget>[
-          TextButton.icon(
-            onPressed: () {
-              Navigator.pushNamed(context, '/location');
-            },
-            icon: const Icon(Icons.edit_location),
-            label: const Text('Edit Location'))
-        ])
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(0, 120.0, 0, 0),
+        child: SafeArea(
+          child: Column(children: <Widget>[
+            TextButton.icon(
+              onPressed: () {
+                Navigator.pushNamed(context, '/location');
+              },
+              icon: const Icon(Icons.edit_location),
+              label: const Text('Edit Location')),
+            const SizedBox(height: 20.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  data['location'],
+                  style: const TextStyle(
+                    fontSize: 28.0,
+                    letterSpacing: 2.0,
+                  ),
+                )
+              ],
+            ),
+            const SizedBox(height: 20.0),
+            Text(
+              data['time'],
+              style: const TextStyle(
+                fontSize: 66.0,
+              ),
+            )
+          ])
+        ),
       )
     );
   }
